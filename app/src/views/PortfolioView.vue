66fad4e03121193b9d1d04c612942e706da3ce57
NellitG <kamaunelson157@gmail.com>
2025-10-21 17:16:16 +0300

    Create components expertise, design, card, Patterns, CTA, discover, Testimonial and register the views

---
+++ app/src/views/PortfolioView.vue
@@ -0,0 +1,20 @@
+<template>
+<Expertise />
+<Design />
+<Card />
+<Patterns />
+<Discover />
+<Testimonial />
+<CTA />
+
+</template>
+
+<script setup>
+import Expertise from '@/components/Expertise.vue'
+import Design from '@/components/Design.vue'
+import Card from '../components/Card.vue';
+import Patterns from '../components/Patterns.vue';
+import Discover from '../components/Discover.vue';
+import Testimonial from '../components/Testimonial.vue';
+import CTA from '../components/CTA.vue';
+</script>
