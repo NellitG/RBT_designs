c89310e77e32cada4d9da63ba38080ee830a899d
NellitG <kamaunelson157@gmail.com>
2025-10-29 11:04:39 +0300

    renamed White Tshirt to Tshirt

--- app/src/components/Services.vue
+++ app/src/components/Services.vue
@@ -82,14 +82,14 @@ import { ref, onMounted, onBeforeUnmount } from "vue";
 import hoodie from "@/assets/hoodie.png";
 import Cup from "@/assets/Cup.png";
 import Flask from "@/assets/Flask.png";
-import WhiteTshirt from "@/assets/White Tshirt.png";
+import Tshirt from "@/assets/Tshirt.png";
 
 
   const cards = [
   { title: "Hoodie Printing", image: hoodie, text: "High-quality custom hoodie prints for your brand or events." },
   { title: "Mug & Cup Printing", image: Cup, text: "Durable and vibrant mug printing for gifts or merchandise." },
   { title: "Flask Printing", image: Flask, text: "Personalized flask printing with elegant, lasting finishes." },
-  { title: "T-Shirt Printing", image: WhiteTshirt, text: "Custom t-shirt designs for all occasions and styles." },
+  { title: "T-Shirt Printing", image: Tshirt, text: "Custom t-shirt designs for all occasions and styles." },
 ]
 
 const currentIndex = ref(0);
