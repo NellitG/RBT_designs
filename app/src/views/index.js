3c0a071650809b21abfa8335cada8fb466736bb5
NellitG <kamaunelson157@gmail.com>
2025-10-29 10:32:19 +0300

    register components in routing

--- app/src/views/index.js
+++ app/src/views/index.js
@@ -1,15 +1,15 @@
 import HomeView from "./HomeView.vue";
 import DetailsView from "./DetailsView.vue";
+import LoginView from "./LoginView.vue";
 import PostsView from "./PostsView.vue";
 import PiniaTest from "./PiniaTest.vue";
-import ApiTestView from "./ApiTestView.vue";
-import LandingView from "./LandingView.vue";
+import PortfolioView from "./PortfolioView.vue";
+import ProcessView from "./ProcessView.vue";
+import ShopView from "./ShopView.vue";
+import ContactView from "./ContactView.vue";
+import DestinationView from "./DestinationView.vue";
+import PicturesView from "./PicturesView.vue";
+import BookView from "./BookView.vue";
+import LearnView from "./LearnView.vue";
 
-export {
-  HomeView,
-  DetailsView,
-  PostsView,
-  PiniaTest,
-  ApiTestView,
-  LandingView,
-};
+export { HomeView, DetailsView, LoginView, PostsView, PiniaTest, PortfolioView, ProcessView, ShopView, ContactView, DestinationView, PicturesView, BookView, LearnView };
