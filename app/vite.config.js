aaf27902bb5d53e8decd61d9685c535bf42ee203
NellitG <kamaunelson157@gmail.com>
2025-10-29 10:51:27 +0300

    corrected code

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,14 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite'
-import { fileURLToPath, URL } from 'url';
+// import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath, URL } from 'url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [
+    vue(),
+    // tailwindcss()
+  ],
   resolve: {
     alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-}
+  },
+  css: {
+    postcss: './postcss.config.js', 
+  }
 })
